5141c053215578bcad2dc00d46a01714a4e9dd85
Marko Lovic <markolovic@hotmail.com>
2014-11-01 02:18:02 -0600

    Write assessment model spec

--- spec/models/assessment_spec.rb
+++ spec/models/assessment_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 describe Assessment do
-  it "should be valid"
+
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:category_id) } # necessary ?
+  it { should validate_numericality_of(:rep) }
+
+  it "should be valid" do
+    #FactoryGirl.reload
+    ass = build(:assessment)
+    expect(ass.save).to eq true
+    # expect(ass).to be_valid
+  end
+
   context "should not be valid if" do
-    it "without associated category"
-    it "without rep"
+    it "rep is zero" do
+      ass = build(:assessment, rep: 0)
+      expect(ass.save).to eq false
+    end
+    it "rep is negative" do
+      ass = build(:assessment, rep: -1)
+      expect(ass.save).to eq false
+    end
   end
 end
