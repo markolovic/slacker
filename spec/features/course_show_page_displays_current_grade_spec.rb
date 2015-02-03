83c488a25dde0dd3a131b6d0344eec33a10d691b
Marko Lovic <markolovic@hotmail.com>
2015-02-04 00:58:25 +0100

    Fix Course current_grade acceptance test and corresponding view

--- spec/features/course_show_page_displays_current_grade_spec.rb
+++ spec/features/course_show_page_displays_current_grade_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 feature "Course show page displays current class grade" do
   scenario "successfully" do
-    course = create(:course)
+    course = course_with_assessments # hardcodes course grade to 62
     visit course_path(course.id)
-    expect(page).to have_css "small", text: course.current_grade
+    expect(course.current_grade).to eq 0.62
+    expect(page).to have_css "small", text: "62.0%"
   end
 end
--- spec/support/model_helper.rb
+++ spec/support/model_helper.rb
@@ -6,3 +6,13 @@ def category_with_assessments(grade, weight)
   cat
   #TODO turn into factory?
 end
+
+def course_with_assessments
+    course = create(:course)
+    course.categories << category_with_assessments(0.5, 0.4)
+    course.categories << category_with_assessments(0.7, 0.6)
+    # Course grade here is hardcoded above to 0.62
+    course.save 
+    course # Necessary?
+    # Probably bad idea
+end
